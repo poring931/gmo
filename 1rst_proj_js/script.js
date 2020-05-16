6c026238b56a81058a547a4aeda4b7ef018e4ffb
poring93 <poring.m@mail.ru>
2020-05-17 00:55:38 +0300

    test

---
+++ 1rst_proj_js/script.js
@@ -0,0 +1,14 @@
+function cal(a, b) {
+    return (a + b);
+}
+
+console.log(cal(3, 55));
+console.log(cal(22, 33));
+
+function retVar() {
+    let num = 50;
+    return num;
+}
+
+let anotherNum = retVar();
+console.log((anotherNum));
