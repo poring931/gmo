3cd9d7b0da9537df81e45ad86f175aa7817139ac
poring93 <poring.m@mail.ru>
2020-06-21 11:45:43 +0300

    promise try

---
+++ 1rst_proj_js/promise_try/promise.js
@@ -0,0 +1,35 @@
+let drink = 1;
+
+function shoot(arrow) {
+    console.log('Вы сделали выстрел....');
+    let promise = new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            Math.random() > .5 ? resolve({}) : reject('missed');
+        },1000);
+    })
+return promise;
+
+}
+
+function win() {
+    console.log('Вы победилил');
+    (drink == 1) ? buyBeer() : giveMoney();
+}
+
+function buyBeer() {
+    console.log('Вам купили пивандрик')
+}
+
+function giveMoney() {
+console.log('Вам закинули бабки')
+}
+function loose(){
+    console.log('Вы проиграли');
+}
+
+
+
+shoot({})
+.then(mark=>console.log('Вы попали в цель'))
+.then(win)
+.catch(loose)
