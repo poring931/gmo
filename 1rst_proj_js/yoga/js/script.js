c23061f789981d1d54056a0c9ef297e2719a4b7c
poring93 <poring.m@mail.ru>
2020-06-23 00:11:31 +0300

    calt try

--- 1rst_proj_js/yoga/js/script.js
+++ 1rst_proj_js/yoga/js/script.js
@@ -252,4 +252,15 @@ window.addEventListener('DOMContentLoaded',function () {
         }
     })
 
+    //calculator
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+    totalValue.innerHTML = 0;
 })
